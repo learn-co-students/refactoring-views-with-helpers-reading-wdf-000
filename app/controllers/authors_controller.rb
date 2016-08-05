a9fdc31e6e0d403b3721d0f1b4efbc6af2d5cbfb
blessedsoy <unmigong@gmail.com>
2016-08-05 16:49:53 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,8 @@
+require 'pry'
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+
+    params[:id] ? @author = Author.find(params[:id]) : @author = Author.new
+
   end
 end
